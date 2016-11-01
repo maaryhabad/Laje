8072ca9a2a6fd16917d8228d97f0e93e47cc38b6
maaryhabad <maryh.abad@gmail.com>
2016-11-01 00:08:02 -0200

    Plataformas

--- Assets/Scripts/Spectrum.cs
+++ Assets/Scripts/Spectrum.cs
@@ -10,18 +10,22 @@ public class Spectrum : MonoBehaviour {
     public float spacing = 1f;
     public int numberOfObjects = 60;
     public GameObject[] Platform1;
+    public Vector3 pos = new Vector3();
+    public Vector2 originPosition;
 
     void Start()
     {
-        for (int y = 0; y < 3; y++)
+        for (int y = 0; y < 5; y++) //onde tá o 3 devia ser o numberOfObjects
         {
-            for (int x = 0; x < 3; x++)
+            for (int x = 0; x < 5; x++)
             {
-                Vector3 pos = new Vector3(x, -5 / spacing, y) * spacing;
+                pos = new Vector3(x, -5 / spacing, y) * spacing; //antes z era igual a y
                 Instantiate(prefab, pos, Quaternion.identity);
+
             }
         }
         Platform1 = GameObject.FindGameObjectsWithTag("Platform1");
+        originPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -32,8 +36,10 @@ public class Spectrum : MonoBehaviour {
         {
             Vector3 previousScale = Platform1[i].transform.localScale;
             previousScale.y = Mathf.Lerp(previousScale.y, spectrum[i] * 3, Time.deltaTime * 30);
+            originPosition = originPosition + pos;
+            Instantiate(Platform1, originPosition, Quaternion.identity);
+            
 
-            //Platform1[i].transform.localScale = previousScale; 
             Platform1[i].transform.DOScaleY(previousScale.y, 2f);
         }
     }
---
+++ Assets/spawnPlatform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnPlatform : MonoBehaviour {
+    public int numberOfObjects = 55;
+    public GameObject Platform;
+
+    private Vector2 originPosition;
+
+	// Use this for initialization
+	void Start () {
+       // Platform = GameObject.FindGameObjectWithTag("Platform1");//isso aqui é o spawn?
+       
+
+	}
+
+    // Update is called once per frame
+    void Spawn() { }       // for (int i = 0; i < numberOfObjects; i++)}
+}
